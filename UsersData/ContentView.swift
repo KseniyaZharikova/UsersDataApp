4db611bbf7fe1823e7dc32e133cc4bcd9d4e9f46
Kseniya Zharikova <kzharikova@ronasit.com>
2022-09-23 14:04:47 +0600

    Initial Commit

---
+++ UsersData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsersData
+//
+//  Created by Kseniya Zharikova on 23/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UsersData/UsersDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersDataApp.swift
+//  UsersData
+//
+//  Created by Kseniya Zharikova on 23/9/22.
+//
+
+import SwiftUI
+
+@main
+struct UsersDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
