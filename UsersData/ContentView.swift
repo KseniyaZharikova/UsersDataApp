a8a63af98985023a144e99a2358919c5abb7248f
Kseniya Zharikova <kzharikova@ronasit.com>
2022-09-28 19:39:02 +0600

    move view to Screen enum

--- UsersData/ContentView.swift
+++ UsersData/ContentView.swift
@@ -7,35 +7,8 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @StateObject private var viewModel = ViewModel()
-    
-    var body: some View {
-        NavigationView {
-            userView
-         .navigationTitle("Users")
-        }
-    }
-    
-    var userView: some View {
-        List(viewModel.users, id:\.id) { user in
-            DisclosureGroup {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Language: \(user.lang)")
-                    Text("Os: \(user.os)")
-                    Text("Played Demo: \(user.playedDemoText)")
-                }
-            } label: {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Id: \(user.id)")
-                }
-            }
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
+struct Users_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Users.Screen(viewModel: .init())
     }
 }
